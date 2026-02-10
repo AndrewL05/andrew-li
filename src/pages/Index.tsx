ff770b24e65506820465607d1c653a5bd1ca61a2
Andrew <liandrew1234@gmail.com>
2026-02-09 21:23:06 -0500

    remove main browser scroll

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Browser from "../components/Browser";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-[#0f0f0f] p-3 md:p-6 flex items-start md:items-center justify-center">
+    <div className="h-full bg-[#0f0f0f] p-3 md:p-6 flex items-center justify-center overflow-hidden">
       <Browser />
     </div>
   );
